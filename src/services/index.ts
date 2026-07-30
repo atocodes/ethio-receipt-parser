b88ea62ca8195921a1f09a7232b55f5fc9b5cbb7
atocodes <atocodes@gmail.com>
2026-07-30 20:50:51 +0300

    fix: reorder export statements for consistency in index.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -4,14 +4,3 @@ export * from "./cbe";
 export * from "./boa";
 export * from "./telebirr";
 export * from "./awash";
-
-async function main() {
-  const res = await handleTelebirrReciept(
-    "https://transactioninfo.ethiotelecom.et/receipt/DGR5AG6NGJ",
-  );
-
-  console.log(res)
-}
-
-
-main()
